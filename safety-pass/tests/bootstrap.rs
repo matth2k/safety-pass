ca9efd2e9ec45a046e34dd61a2b3451fced5a426
matth2k <mrh259@cornell.edu>
2026-08-04 21:19:01 -0400

    Add a test to demonstrate capabilities

---
+++ safety-pass/tests/bootstrap.rs
@@ -0,0 +1,64 @@
+use safety_net::{Identifier, Instantiable, Net, Netlist, assert_verilog_eq};
+use safety_pass::{Cell, ModInst, ModOrCell};
+use std::rc::Rc;
+
+type Inst = ModOrCell<Cell>;
+
+fn passthru_nl<I: Instantiable>(id: Identifier) -> Rc<Netlist<I>> {
+    let nl = Netlist::new(id);
+
+    let x = nl.insert_input(Net::new_logic("x".into()));
+
+    x.expose_with_name("y".into());
+
+    nl
+}
+
+#[test]
+fn test_nesting() {
+    let outer: Rc<Netlist<Inst>> = passthru_nl("outer".into());
+    let inner: Rc<Netlist<Cell>> = passthru_nl("inner".into());
+
+    let inst = ModOrCell::ModInst(ModInst::new(&inner));
+
+    let nr = outer.insert_gate_disconnected(inst, "inst".into());
+
+    let oin = outer.first().unwrap().get_output(0);
+    let oin = outer.replace_net_uses(oin, &nr.get_output(0)).unwrap();
+    nr.get_input(0).connect(oin);
+
+    let verilog = inner.to_string() + "\n" + &outer.to_string();
+
+    assert_verilog_eq!(
+        verilog,
+        "module inner (
+           x,
+           y
+         );
+           input wire x;
+           output wire y;
+         
+         
+           assign y = x;
+         
+         endmodule
+         
+         module outer (
+           x,
+           y
+         );
+           input wire x;
+           output wire y;
+           wire inst_y;
+         
+           inner inst (
+             .x(x),
+             .y(inst_y)
+           );
+         
+           assign y = inst_y;
+         
+         endmodule"
+            .to_string()
+    );
+}
