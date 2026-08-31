d162093c62a86569303108f59bd58e15b8253874
matth2k <mrh259@cornell.edu>
2026-08-31 13:20:46 -0400

    Remove subtractor

--- tutorial/rca_main.cpp
+++ tutorial/rca_main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char **argv) {
   Vrca *top = new Vrca{contextp};
 
   // Test addition
-  top->subtract = 0;
   for (int i = 0; i < 16; ++i) {
     for (int j = 0; j < 16; ++j) {
       top->a = i;
@@ -23,25 +22,6 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Test subtraction
-  top->subtract = 1;
-  for (int i = 0; i < 16; ++i) {
-    for (int j = 0; j < 16; ++j) {
-      top->a = i;
-      top->b = j;
-      top->eval();
-      int shift = sizeof(int) * 8 - 4;
-      int result = ((top->sum) << shift) >> shift;
-      if (result != (i - j)) {
-        printf("ERROR: %d - %d != %d\n", i, j, result);
-        delete top;
-        delete contextp;
-        return 1;
-      } else {
-        printf("OK: %d - %d = %d\n", i, j, result);
-      }
-    }
-  }
   delete top;
   delete contextp;
   return 0;
